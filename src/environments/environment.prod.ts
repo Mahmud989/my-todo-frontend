fcbc87c4ff2bf48689475c016737566c6eca57dd
Mahmud Bodurov <bodurovmahmud@gmail.com>
2021-12-16 12:43:22 +0400

    Dockerfile change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+export const API_URL = 'https://todo.letsecure.az/api/v1'
+
 export const environment = {
-  production: true
+  production: true,
+  SERVICE: {
+    ALL_TODOs: API_URL + '/all',
+    CREATE_TODO: API_URL + '/create',
+  }
 };
